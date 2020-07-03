5de21573eecf9568cb94501743a80722228fad27
Alex <alexlong2001@outlook.com>
2020-07-03 08:05:24 +0100

    v3 start

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,46 +3,17 @@ import Nav from '../compounds/Nav.svelte'
 import Footer from '../compounds/Footer.svelte'
 </script>
 
-<div class="bg-darkest-primary">
-	<div class="max-w-64rem mx-auto bg-duckegg overflow-x-hidden">
-		  <div class="theme-switch">
-    		<div class="switch"></div>
-  		</div>
-		<!-- <div class="bg-primary text-center py-4">
-			<a href="https://alex-long.co.uk/hostingsolutions/v1/hosting.html" class="text-white text-lg underline text-bright-primary">Click here to visit v1.0</a>
-		</div> -->
-		<Nav />
+<Nav />
 
-		<main>
-			<slot></slot>
-		</main>
+<main>
+	<slot></slot>
+</main>
 
-		<Footer />
-	</div>
-</div>
+<Footer />
 
 <style>
-.theme-switch {
-  --background: #FFF;
-  --text: #000;
-  color: var(--text);
-  width: 70px;
-  height: 30px;
-  background: var(--highlight);
-  border-radius: 50px;
-  position: relative;
-}
-
-.switch {
- background: white;
- width: 24px;
- height: 24px;
- background: var(--background);
- border-radius: 100%;
- position: absolute;
- top: 3px;
- left: 4px;
- transition: 0.5s all ease;
+:global(body) {
+	font-family: Poppins, sans-serif;
 }
 </style>
 
--- tailwind.js
+++ tailwind.js
@@ -5,48 +5,15 @@ module.exports = {
     },
     extend: {
       screens: {
-        '700px': '700px',
         '768px': '768px',
-        '850px': '850px',
-        '1024px': '1024px',
-        '1440px': '1440px'
+        '1024px': '1024px'
       },
       colors: {
-        'duckegg': '#F2FFFE',
-        'primary': '#006572',
-        'secondary': '#3E3E3E',
-        'bright-primary': '#00FFF0',
-        'dark-primary': '#003b42',
-        'darkest-primary': '#001f23'
+        'primary': '#006572'
       },
       width: {
-        '30%': '30%',
-        '32%': '32%',
-        '45%': '45%',
-        '48%': '48%',
-        '88%': '88%',
-        '18rem': '18rem',
-        '20rem': '20rem',
-        '22rem': '22rem',
-        '24rem': '24rem',
-        '25rem': '25rem',
-        '48rem': '48.25rem'
       },
       maxWidth: {
-        '20rem': '20rem',
-        '22rem': '22rem',
-        '24rem': '24rem',
-        '28rem': '28rem',
-        '32rem': '32rem',
-        '40rem': '40rem',
-        '48rem': '49rem',
-        '53rem': '53rem',
-        '62rem': '62rem',
-        '64rem': '64rem',
-        '68rem': '68rem'
-      },
-      boxShadow: {
-        'md-top' : 'box-shadow: 4px 0px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)'
       }
     }
   },
