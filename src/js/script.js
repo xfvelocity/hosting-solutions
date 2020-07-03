4616793d25fa9180d3b3a08434be4b07dd350cb6
Alex Long <alexlong2001@outlook.com>
2020-07-03 17:07:00 +0100

    dropdown and responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,25 @@
 const navButton = document.querySelector('.navbutton');
 const navLinks = document.querySelector('.nav-links')
+let navPressed;
 
 navButton.addEventListener('click', () => {
+    navPressed = !navPressed;
+    navLinks.classList.remove('nav-links-open');
+
+    if(navPressed) {
+        navLinks.classList.toggle('nav-links-open');
+    } 
 })
 
-if(navLinks) {
-    navLinks.classList.toggle('nav-links-open')
-}
+const dropdownButton = document.querySelector('.features');
+const dropdown = document.querySelector('.dropdown');
+let dropPressed;
+
+dropdownButton.addEventListener('click', () => {
+    dropPressed = !dropPressed;
+    dropdown.classList.remove('drop-pressed');
+
+    if(dropPressed) {
+        dropdown.classList.toggle('drop-pressed');
+    } 
+})
