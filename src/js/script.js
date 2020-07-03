f56c372b94b7ee904ebf563ee1b880c8f7a3094d
Alex <alexlong2001@outlook.com>
2020-07-03 10:48:46 +0100

    v3

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+const navButton = document.querySelector('.navbutton');
+const navLinks = document.querySelector('.nav-links')
+
+navButton.addEventListener('click', () => {
+})
+
+if(navLinks) {
+    navLinks.classList.toggle('nav-links-open')
+}
